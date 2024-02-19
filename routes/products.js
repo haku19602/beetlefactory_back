7959e080f523c7052915588cacb9f47014dbf75b
haku19602 <haku19602@gmail.com>
2024-02-19 23:18:32 +0800

    取得單一商品頁面的路由

--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,16 @@
 import { Router } from 'express'
 import * as auth from '../middlewares/auth.js'
-import { create, getAll, edit, remove, get } from '../controllers/products.js'
+import { create, getAll, edit, remove, get, getId } from '../controllers/products.js'
 import upload from '../middlewares/upload.js'
 import admin from '../middlewares/admin.js'
 
 const router = Router()
 
 router.post('/', auth.jwt, admin, upload, create)
-router.get('/all', auth.jwt, admin, getAll)
-router.patch('/:id', auth.jwt, admin, upload, edit)
-router.get('/', get)
-// router.get('/:id', getId)
+router.get('/all', auth.jwt, admin, getAll) // 取得所有商品頁面 -> 管理者用
+router.patch('/:id', auth.jwt, admin, upload, edit) // 編輯商品 -> 管理者用
+router.get('/', get) // 取得上架商品頁面 -> 一般使用者用
+router.get('/:id', getId) // 取得單一商品頁面
 // ----------
 router.delete('/:id', auth.jwt, admin, remove)
 
